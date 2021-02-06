2ed2f761d7366e1df60e26122f8dd8328cd1cb6e
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-05 19:44:16 -0500

    Player and Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 4.0f;
+
+    //Buena practica guardar el movimiento horizontal y vertical
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //d = v * t
+
+        if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+        {
+            this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) 
+                * walkSpeed * Time.deltaTime, 0, 0));
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
+        {
+            this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical)
+                * walkSpeed * Time.deltaTime, 0));
+        }
+    }
+}
