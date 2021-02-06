065db8e9443ebddb78158064a6c609a13bef6390
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-05 21:00:45 -0500

    Blend Trees Configurations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 4.0f;
+    private bool walking = false;
+    public Vector2 lastMovement = Vector2.zero;
 
-    //Buena practica guardar el movimiento horizontal y vertical
+    //Buena practica guardar el movimiento horizontal y vertical, asi se carga una vez y no hay que cargarlos en el update
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+    private const string walkingState = "Walking";
 
     private Animator anim;
 
@@ -22,20 +27,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //d = v * t
+        walking = false;
 
         if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
             this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) 
                 * walkSpeed * Time.deltaTime, 0, 0));
+            walking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
             this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical)
                 * walkSpeed * Time.deltaTime, 0));
+            walking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
 
         anim.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
         anim.SetFloat(vertical, Input.GetAxisRaw(vertical));
+        anim.SetBool(walkingState, walking);
+        anim.SetFloat(lastHorizontal, lastMovement.x);
+        anim.SetFloat(lastVertical, lastMovement.y);
     }
 }
