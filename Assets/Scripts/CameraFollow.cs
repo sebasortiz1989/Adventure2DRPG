5faf4e231ff854429721206fb52c63fda5bb909a
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-06 20:12:31 -0500

    Camera Programming

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject followTarget;
+    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private float cameraSpeed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(followTarget.transform.position.x, 
+                                     followTarget.transform.position.y, -10);
+        this.transform.position = Vector3.Lerp(this.transform.position, 
+                                                targetPosition, cameraSpeed * Time.deltaTime); //Lerp is linear interpolation
+    }
+}
