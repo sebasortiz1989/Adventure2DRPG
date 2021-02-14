0bbbc669fd3f3a1a9474067ea9082db9f57a5f6d
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-14 07:14:04 -0500

    Health Manager for Player

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,40 +4,13 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-    public float timeToRevivePlayer;
-    private float timeRevivalCounter;
-    private bool playerReviving;
-
-    private GameObject thePlayer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerReviving)
-        {
-            timeRevivalCounter -= Time.deltaTime;
-            if (timeRevivalCounter < 0)
-            {
-                playerReviving = false;
-                thePlayer.SetActive(true);
-            }
-        }
-    }
+    [SerializeField] int damage = 1;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            collision.gameObject.SetActive(false);
-            playerReviving = true;
-            timeRevivalCounter = timeToRevivePlayer;
-            thePlayer = collision.gameObject;
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
         }
     }
 }
