905d7017c595ed626855a25c652f3b992dd57a9a
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-14 16:17:52 -0500

    Particle effects

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class WeaponDamage : MonoBehaviour
 {
     [SerializeField] int damage = 10;
+    [SerializeField] GameObject hurtAnimation;
+    [SerializeField] GameObject hitPoint;
+
+    private GameObject blood;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Enemy"))
         {
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+            blood = Instantiate(hurtAnimation, hitPoint.transform.position, hitPoint.transform.rotation);
+            Destroy(blood, 1.5f);
         }
     }
 }
