b2fd2328a08fe1b3c10fee01074f9fb92d7cbb05
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-13 20:07:25 -0500

    Programming physics and enemy patrol

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float enemySpeed = 1;
+    public float timeBetweenSteps;
+    public float timeToMakeStep;
+    public Vector2 directionToMakeStep;
+
+    private Rigidbody2D enemyRigidBody;
+    private bool isMoving;
+    private float timeBetweenStepsCounter;
+    private float timeToMakeStepCounter;
+    private Animator enemyAnimator;
+
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        enemyAnimator = GetComponent<Animator>();
+
+        timeBetweenStepsCounter = timeBetweenSteps;
+        timeToMakeStepCounter = timeToMakeStep;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            timeToMakeStepCounter -= Time.deltaTime;
+            enemyRigidBody.velocity = directionToMakeStep;
+
+            if (timeToMakeStepCounter < 0)
+            {
+                isMoving = false;
+                timeBetweenStepsCounter = timeBetweenSteps;
+                enemyRigidBody.velocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            timeBetweenStepsCounter -= Time.deltaTime;
+            if (timeBetweenStepsCounter < 0)
+            {
+                isMoving = true;
+                timeToMakeStepCounter = timeBetweenSteps;
+                directionToMakeStep = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2) * enemySpeed);
+            }
+        }
+
+        enemyAnimator.SetFloat(horizontal, directionToMakeStep.x);
+        enemyAnimator.SetFloat(vertical, directionToMakeStep.y);
+    }
+}
