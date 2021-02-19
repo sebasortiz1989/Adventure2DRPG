db6509d030d3fad12b6bba251d654df43c6d7f74
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-18 21:19:47 -0500

    First Dialog for the NPC

---
+++ Assets/Scripts/NPCDialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialog : MonoBehaviour
+{
+    public string dialog;
+    private DialogManager manager;
+    private bool playerInTheZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<DialogManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInTheZone && Input.GetKeyDown(KeyCode.Return))
+        {
+            manager.ShowDialog(dialog);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInTheZone = true;
+        }
+    }
+}
