0a6092fd98746b437e47376e5e4b455e5aa93c7f
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-02-21 15:52:10 -0500

    Add music to our game

---
+++ Assets/Scripts/AudioNewTrack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioNewTrack : MonoBehaviour
+{
+    private AudioManager manager;
+    public int newTrackID;
+    public bool playOnStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<AudioManager>();
+        if (playOnStart)
+        {
+            manager.PlayNewTrack(newTrackID);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            manager.PlayNewTrack(newTrackID);
+            gameObject.SetActive(false); 
+        }
+    }
+}
